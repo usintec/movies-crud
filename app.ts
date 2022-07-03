b19cf589715daf719808e7036522a8c6d3059096
usintec <34805186+usintec@users.noreply.github.com>
2022-07-03 08:59:24 -0700

    Update app.ts

--- app.ts
+++ app.ts
@@ -15,7 +15,7 @@ class App {
         app.use(bodyParser.json());
         app.use((req, res, next) => {
             res.header('Access-Control-Allow-Origin', '*');
-            res.header('Access-Control-Allow-Metheds', 'GET,POST,PUT,PATCH,DELETE');
+            res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
             res.header('Access-Control-Allow-Headers', 'Content-Type');
             next();
         });
